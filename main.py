ddb575db93bdfe63dee7533073c2fb4e435b1ff1
KoryuDaichi <mcginnis.cole@gmail.com>
2025-02-26 04:58:16 -0600

    Change Main.py to print word count.

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ def get_book_text(bookpath):
         book_contents = f.read()
     return book_contents
 
+def word_count(text):
+    wordsplit = text.split()
+    wordcount = 0
+    for word in wordsplit:
+        wordcount += 1
+    return wordcount
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    num_words = word_count(text)
+    print(f"{num_words} words found in the document")
 
 main()

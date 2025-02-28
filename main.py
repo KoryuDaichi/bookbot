4eb1a1bbfa0e2f827a5a3eae263fcc72a5a5539d
KoryuDaichi <mcginnis.cole@gmail.com>
2025-02-27 23:19:06 -0600

    add function to move the character count to a sorted list

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ def get_book_text(bookpath):
 
 from stats import get_num_words
 from stats import get_character_count
+#from stats import character_count_sort
+from stats import character_count_to_sorted_list
+
 
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
     character_count = get_character_count(text)
-    print(character_count)
+    charactercountdictionary = character_count_to_sorted_list(character_count)
+    print(charactercountdictionary)
+    
 
 main()
--- stats.py
+++ stats.py
@@ -15,3 +15,21 @@ def get_character_count(words):
         else :
             characternumbers[c] += 1
     return characternumbers
+
+def sort_on(dict):
+    return dict["count"]
+
+def character_count_to_sorted_list(charactercount):
+    charactercountlist = []
+    for char, count in charactercount.items():
+        charactercountlist.append({"char": char, "count":count})
+    charactercountlist.sort(key=lambda x: x["count"],reverse=True)
+    return charactercountlist
+
+#def character_count_sort(charactercountlist):
+#    charactercountlist.sort(key=sort_on,reverse=True)
+#    return charactercountlist   
+
+#test_dict = {'t': 29493, 'h': 19176, 'e': 44538, ' ': 70480}
+#sorted_list = character_count_to_list(test_dict)
+#print(sorted_list)

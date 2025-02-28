87c7dc8749f4b4d892398091fd895fcd81f156ef
KoryuDaichi <mcginnis.cole@gmail.com>
2025-02-27 23:51:37 -0600

    Shorten and simplify wordcount code

--- stats.py
+++ stats.py
@@ -1,9 +1,6 @@
 def get_num_words(text):
     wordsplit = text.split()
-    wordcount = 0
-    for word in wordsplit:
-        wordcount += 1
-    return wordcount
+    return len(wordsplit)
 
 def get_character_count(words):
     lowerwords = words.lower()
